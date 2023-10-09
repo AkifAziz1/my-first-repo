ab7d9c00b471ae2c973170c723aaa86558feffe7
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-10-09 19:28:50 +0400

    +

--- src/Ikinci_Ay_Telimi/Main.java
+++ src/Ikinci_Ay_Telimi/Main.java
@@ -22,6 +22,7 @@ public class Main {
                                      sistem basha chatir*/
             MenuUtil.processMenu(menu);
         }
+
     }
 
     public static void showSuccessOperMessage() {
