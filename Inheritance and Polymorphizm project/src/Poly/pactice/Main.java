bb06399bcf17ad7727de95222c0aff6ffcc91b6a
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-11-03 01:54:14 +0400

    +

--- Inheritance and Polymorphizm project/src/Poly/pactice/Main.java
+++ Inheritance and Polymorphizm project/src/Poly/pactice/Main.java
@@ -1,11 +1,20 @@
 package Poly.pactice;
 
-public class Main {
+import java.util.Scanner;
+
+public class Main  {
 
     public static void main(String[] args) {
-        BMW b = new BMW();
+//        BMW b = new BMW();
+//
+//        Car c = b;
+//        c.drive();
 
-        Car c = b;
-        c.drive(); 
+        String s ;
+        Scanner sc = new Scanner(System.in);
+        s = sc.next();
+
+        boolean b = s.equals("Java");
+        System.out.println(b);
     }
 }
