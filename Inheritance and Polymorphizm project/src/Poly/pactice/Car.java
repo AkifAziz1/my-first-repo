fa896ec5bea8c1c42934a61df146ec4fcf060a0f
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-11-09 00:52:58 +0400

    +

--- Inheritance and Polymorphizm project/src/Poly/pactice/Car.java
+++ Inheritance and Polymorphizm project/src/Poly/pactice/Car.java
@@ -1,7 +1,13 @@
 package Poly.pactice;
 
+import java.util.Random;
+
 public class Car {
 
+    public static void main(String[] args) {
+        Random r = new Random();
+        System.out.println(r.nextInt(10));
+    }
     public  int a = 2;
 
     public void drive(){
