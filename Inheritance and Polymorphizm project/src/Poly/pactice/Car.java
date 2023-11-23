fecb537b3659f0a6e40c9b453938ab64de5ec915
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-11-23 23:48:11 +0400

    +

--- Inheritance and Polymorphizm project/src/Poly/pactice/Car.java
+++ Inheritance and Polymorphizm project/src/Poly/pactice/Car.java
@@ -6,9 +6,10 @@ public class Car {
     public static void main(String[] input) {
         int plan = 1;
         plan = plan++ + --plan;
-        if(plan==1) {
+        if (plan == 1) {
             System.out.print("Plan A");
-        } else { if(plan==2) System.out.print("Plan B");
+        } else {
+            if (plan == 2) System.out.print("Plan B");
         } else System.out.print("Plan C");
     }
 
