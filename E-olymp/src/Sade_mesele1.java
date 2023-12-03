88d4a67d586306dc211c7367e74b9b9aed481e4c
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-12-03 23:40:12 +0400

    +

--- E-olymp/src/Sade_mesele1.java
+++ E-olymp/src/Sade_mesele1.java
@@ -2,6 +2,6 @@ public class Sade_mesele1 {
     public static void main(String[] args) {
         int a = 2;
         int b= 3;
-        System.out.println(a + "" + b);
+        System.out.println(a + " " + b);
     }
 }
--- Inheritance and Polymorphizm project/src/Poly/pactice/Car.java
+++ Inheritance and Polymorphizm project/src/Poly/pactice/Car.java
@@ -4,6 +4,16 @@ import java.util.Random;
 
 
 public class Car {
+    static void sumTest(int ... v){
+        System.out.println(v.length);
+
+    }
+
+    public static void main(String[] args) {
+        sumTest();
+        sumTest(3);
+        sumTest(1,2,3,4);
+    }
 
 
     public void drive() {
