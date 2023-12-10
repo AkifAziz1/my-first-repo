5dd51581928eada9910e7fc7c4208f9ca94695f7
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-12-10 23:41:01 +0400

    +

--- Inheritance and Polymorphizm project/src/Poly/pactice/BMW.java
+++ Inheritance and Polymorphizm project/src/Poly/pactice/BMW.java
@@ -1,15 +1,10 @@
 package Poly.pactice;
 
 public class BMW extends Car {
-    public static void main(String[] args) {
-        String[] days = new String[]{"Sunday", "Monday", "Tuesday",
-                "Wednesday", "Thursday", "Friday", "Saturday"};
-        for(int i = 1; i<days.length;i++)
-            System.out.println(days[i]);
-    }
 
 
-    public void drive() {
-        System.out.println("BMW Drive");
+
+        public void drive () {
+            System.out.println("BMW Drive");
+        }
     }
-}
--- Inheritance and Polymorphizm project/src/Poly/pactice/Mercedes.java
+++ Inheritance and Polymorphizm project/src/Poly/pactice/Mercedes.java
@@ -1,4 +1,12 @@
 package Poly.pactice;
 
 public class Mercedes extends Car{
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+// for-each döngüsü ile dizideki her bir elemanı yazdırma
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
 }
