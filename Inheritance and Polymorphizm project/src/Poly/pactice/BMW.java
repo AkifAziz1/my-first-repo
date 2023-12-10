b0cfbf83396dc56df72c04a070f65dd6dc138c6a
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-12-10 15:14:22 +0400

    +

--- Inheritance and Polymorphizm project/src/Poly/pactice/BMW.java
+++ Inheritance and Polymorphizm project/src/Poly/pactice/BMW.java
@@ -1,7 +1,13 @@
 package Poly.pactice;
 
 public class BMW extends Car {
-    public int a = 1;
+    public static void main(String[] args) {
+        String[] days = new String[]{"Sunday", "Monday", "Tuesday",
+                "Wednesday", "Thursday", "Friday", "Saturday"};
+        for(int i = 1; i<days.length;i++)
+            System.out.println(days[i]);
+    }
+
 
     public void drive() {
         System.out.println("BMW Drive");
