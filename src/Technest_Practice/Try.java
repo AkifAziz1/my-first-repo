e6ce8fc4ef7e4b85fb1d13b5a18fb7acdd9dcc4b
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-12-12 02:48:26 +0400

    +

---
+++ src/Technest_Practice/Try.java
@@ -0,0 +1,13 @@
+package Technest_Practice;
+
+public class Try {
+    public static void main(String[] args) {
+        String lol = "lol";
+        System.out.println(lol.toUpperCase() == lol);
+        System.out.println(lol.toUpperCase() == lol.toUpperCase());
+        System.out.println(lol.toUpperCase().equals(lol));
+        System.out.println(lol.toUpperCase().equals(lol.toUpperCase()));
+        System.out.println(lol.toUpperCase().equalsIgnoreCase(lol));
+        System.out.println(lol.toUpperCase().equalsIgnoreCase(lol.toUpperCase()));
+    }
+}
