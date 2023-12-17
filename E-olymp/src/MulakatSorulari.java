03e03f2199f4a0fd2adf86b479bd2bc9503af1bd
AkifAziz1 <aki.azizov.2014@gmail.com>
2023-12-18 00:23:24 +0400

    +

--- E-olymp/src/MulakatSorulari.java
+++ E-olymp/src/MulakatSorulari.java
@@ -1,16 +1,25 @@
 
 public class MulakatSorulari {
     public String Sum(String val1, String val2){  //bu metoda iki tane tam sayi string olarak gonderilecek ve bu sayilarin geri gonderilmesi gerekiyor
+        int convertVal1= Integer.parseInt(val1);
+        int convertVal2= Integer.parseInt(val2);
+
+        int sum = convertVal1 + convertVal2;
+        System.out.println(sum);
         return null;
     }
 
+
+
     public static void main(String[] args) {
-        MulakatSorulari m = new MulakatSorulari();
-        m.Sum("12","13");
+        String val1 = "123";
+        String val2 = "456";
 
-//        int TransverVal1 = Integer.parseInt(val1);
-//        int TransverVal2 = Integer.parseInt(val2);
+        int convertVal1= Integer.parseInt(val1);
+        int convertVal2= Integer.parseInt(val2);
 
-        System.out.println(TransverVal1 + TransverVal2);
+        int sum = convertVal1 + convertVal2;
+        System.out.println(sum);
+
     }
 }
