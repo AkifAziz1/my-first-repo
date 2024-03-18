e82852ce19373305a89a011fcab01e5f9b988810
AkifAziz1 <aki.azizov.2014@gmail.com>
2024-03-19 00:19:16 +0400

    +

--- E-olymp/src/Sade_mesele1.java
+++ E-olymp/src/Sade_mesele1.java
@@ -3,5 +3,6 @@ public class Sade_mesele1 {
         int a = 2;
         int b= 3;
         System.out.println(a + " " + b);
+
     }
 }
--- E-olymp/src/Sade_mesele2.java
+++ E-olymp/src/Sade_mesele2.java
@@ -1,11 +1,13 @@
 import java.util.Arrays;
-import java.util.Scanner;
+        import java.util.Scanner;
 
 public class Sade_mesele2 {
     public static void main(String[] args) {String[] days = new String[] { "Sunday", "Monday", "Tuesday",
             "Wednesday", "Thursday", "Friday", "Saturday" };
         for (int i = 1; i <= days.length; i++)
             System.out.println(days[i]);
+
     }
+
 }
 
